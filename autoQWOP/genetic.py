a1ecf37368c3b2e237f331714a6e9c9ed294758f
Ryan Massoth <rmassoth82@gmail.com>
2018-05-20 11:14:47 -0400

    Wrote function and tests for crossover.

--- autoQWOP/genetic.py
+++ autoQWOP/genetic.py
@@ -6,8 +6,38 @@ button presses for playing the game QWOP.
 
 import random
 
-class chromosome():
+class Chromosome():
     """
 
-    Represents a single chromosome. To be used in a population.
+    Represents a single chromosome.
     """
+
+    def __init__(self, sequence="", fitness=0.0):
+        self.sequence = sequence
+        self.fitness = fitness
+
+
+def crossover(parent1, parent2, rate):
+    """
+
+    Performs genetic crossover of two chromosomes to create an offspring.
+    """
+    if random.random() < rate:
+        crossover = int(random.random() * len(parent1.sequence))
+        chromo1 = "{}{}".format(
+            parent1.sequence[:crossover],
+            parent2.sequence[crossover:])
+        chromo2 = "{}{}".format(
+            parent2.sequence[:crossover],
+            parent1.sequence[crossover:])
+        return Chromosome(sequence=chromo1), Chromosome(sequence=chromo2)
+    else:
+        return parent1, parent2
+
+
+def mutate():
+    """
+
+    Performs a genetic mutation on a chromosome.
+    """
+    pass
--- tests/test_genetic.py
+++ tests/test_genetic.py
@@ -1,2 +1,33 @@
 import pytest
 
+from autoQWOP.genetic import Chromosome, crossover, mutate
+
+def test_chromo():
+    sequence = "235562513"
+    fitness = 1.2
+    chromo = Chromosome(sequence=sequence, fitness=fitness)
+    assert chromo.sequence == sequence
+    assert chromo.fitness == fitness
+
+def test_crossover():
+    parent1 = Chromosome(sequence="12345")
+    parent2 = Chromosome(sequence="54321")
+    offspring1, offspring2 = crossover(parent1, parent2, 1.1)
+    assert offspring1.sequence != parent1.sequence
+    assert offspring1.sequence != parent2.sequence
+    assert offspring2.sequence != parent1.sequence
+    assert offspring2.sequence != parent2.sequence
+    assert offspring1.sequence != offspring2.sequence
+
+def test_no_crossover():
+    parent1 = Chromosome(sequence="12345")
+    parent2 = Chromosome(sequence="54321")
+    offspring1, offspring2 = crossover(parent1, parent2, 0.0)
+    assert offspring1.sequence == parent1.sequence
+    assert offspring1.sequence != parent2.sequence
+    assert offspring2.sequence != parent1.sequence
+    assert offspring2.sequence == parent2.sequence
+    assert offspring1.sequence != offspring2.sequence
+
+def test_mutate():
+    pass

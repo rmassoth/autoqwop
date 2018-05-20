e0bf14192bd175f55f4959c0c05b1458898c541e
Ryan Massoth <rmassoth82@gmail.com>
2018-05-20 09:11:03 -0400

    Starting to build the genetic algorithm.

---
+++ autoQWOP/genetic.py
@@ -0,0 +1,13 @@
+"""
+
+A simple genetic algorithm for finding the optimal
+button presses for playing the game QWOP.
+"""
+
+import random
+
+class chromosome():
+    """
+
+    Represents a single chromosome. To be used in a population.
+    """

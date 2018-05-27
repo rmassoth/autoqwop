70e3e7020643d35d483a94192c4fbe8907ca0d86
Ryan Massoth <rmassoth82@gmail.com>
2018-05-27 13:16:44 -0400

    Finished mutate method and added random sequence method and tests.

--- autoQWOP/genetic.py
+++ autoQWOP/genetic.py
@@ -17,12 +17,12 @@ class Chromosome():
         self.fitness = fitness
 
 
-def crossover(parent1, parent2, rate):
+def crossover(parent1, parent2, crossover_rate):
     """
 
     Performs genetic crossover of two chromosomes to create an offspring.
     """
-    if random.random() < rate:
+    if random.random() < crossover_rate:
         crossover = int(random.random() * len(parent1.sequence))
         chromo1 = "{}{}".format(
             parent1.sequence[:crossover],
@@ -35,9 +35,28 @@ def crossover(parent1, parent2, rate):
         return parent1, parent2
 
 
-def mutate():
+def mutate(individual, mutation_rate):
     """
 
-    Performs a genetic mutation on a chromosome.
+    Performs a genetic mutation on a chromosome at the specified rate.
     """
-    pass
+    new_chromo = Chromosome(sequence=individual.sequence)
+    if random.random() < mutation_rate:
+        position = random.randrange(len(individual.sequence))
+        mutation = format(random.randrange(16), 'x')
+        sequence_list = list(individual.sequence)
+        sequence_list[position] = mutation
+        new_sequence_string = ''.join(sequence_list)
+        new_chromo.sequence = new_sequence_string
+    return new_chromo
+
+def get_random_sequence(length):
+    """
+
+    Returns a random string of hexadecimal numbers
+    """
+    sequence = ''
+    for i in range(length):
+        random_letter = format(random.randrange(16), 'x')
+        sequence = '{}{}'.format(sequence, random_letter)
+    return sequence
--- tests/test_genetic.py
+++ tests/test_genetic.py
@@ -1,17 +1,21 @@
 import pytest
 
-from autoQWOP.genetic import Chromosome, crossover, mutate
+from autoQWOP.genetic import (
+    Chromosome,
+    crossover,
+    mutate,
+    get_random_sequence)
 
 def test_chromo():
-    sequence = "235562513"
+    sequence = "2355625130"
     fitness = 1.2
     chromo = Chromosome(sequence=sequence, fitness=fitness)
     assert chromo.sequence == sequence
     assert chromo.fitness == fitness
 
 def test_crossover():
-    parent1 = Chromosome(sequence="12345")
-    parent2 = Chromosome(sequence="54321")
+    parent1 = Chromosome(sequence="0123456789ABCDEF")
+    parent2 = Chromosome(sequence="FEDCBA98765543210")
     offspring1, offspring2 = crossover(parent1, parent2, 1.1)
     assert offspring1.sequence != parent1.sequence
     assert offspring1.sequence != parent2.sequence
@@ -20,8 +24,8 @@ def test_crossover():
     assert offspring1.sequence != offspring2.sequence
 
 def test_no_crossover():
-    parent1 = Chromosome(sequence="12345")
-    parent2 = Chromosome(sequence="54321")
+    parent1 = Chromosome(sequence="0123456789ABCDEF")
+    parent2 = Chromosome(sequence="FEDCBA9876543210")
     offspring1, offspring2 = crossover(parent1, parent2, 0.0)
     assert offspring1.sequence == parent1.sequence
     assert offspring1.sequence != parent2.sequence
@@ -30,4 +34,57 @@ def test_no_crossover():
     assert offspring1.sequence != offspring2.sequence
 
 def test_mutate():
-    pass
+    """
+
+    Mutate 5 indiviuals with identical sequences and make sure 
+    that they are mutated in a random way.
+    """
+    sequence = "0123456789ABCDEF"
+    individual1 = Chromosome(sequence=sequence)
+    individual2 = Chromosome(sequence=sequence)
+    individual3 = Chromosome(sequence=sequence)
+    individual4 = Chromosome(sequence=sequence)
+    individual5 = Chromosome(sequence=sequence)
+    mutated1 = mutate(individual1, 1.0)
+    mutated2 = mutate(individual2, 1.0)
+    mutated3 = mutate(individual3, 1.0)
+    mutated4 = mutate(individual4, 1.0)
+    mutated5 = mutate(individual5, 1.0)
+    assert mutated1.sequence != individual1.sequence
+    assert mutated2.sequence != individual2.sequence
+    assert mutated3.sequence != individual3.sequence
+    assert mutated4.sequence != individual4.sequence
+    assert mutated5.sequence != individual5.sequence
+    assert mutated1.sequence != individual2.sequence
+    assert mutated1.sequence != individual3.sequence
+    assert mutated1.sequence != individual4.sequence
+    assert mutated1.sequence != individual5.sequence
+
+def test_no_mutate():
+    sequence = "123456789ABCDEF"
+    individual = Chromosome(sequence=sequence)
+    mutated = mutate(individual, 0.0)
+    assert mutated.sequence == individual.sequence
+
+def test_random_sequence():
+    hex_chars = {'0',
+        '1',
+        '2',
+        '3',
+        '4',
+        '5',
+        '6',
+        '7',
+        '8',
+        '9',
+        'a',
+        'b',
+        'c',
+        'd',
+        'e',
+        'f'}
+    sequence_length = 10
+    sequence = get_random_sequence(sequence_length)
+    assert len(sequence) == sequence_length
+    for letter in sequence:
+        assert letter in hex_chars
